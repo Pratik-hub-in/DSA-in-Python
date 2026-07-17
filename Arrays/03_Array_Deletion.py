afe19c2fa727d370f3460ace32eb5e08e6b31874
Pratik <kokatepratik20@gmail.com>
2026-07-17 23:30:32 +0530

    Add Array Deletion implementation

---
+++ Arrays/03_Array_Deletion.py
@@ -0,0 +1,18 @@
+"""
+Program: Array Deletion
+
+Description:
+Delete an element from an array using its value.
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(1)
+"""
+
+numbers = [10, 20, 30, 40, 50]
+
+numbers.remove(30)
+
+print(numbers)
