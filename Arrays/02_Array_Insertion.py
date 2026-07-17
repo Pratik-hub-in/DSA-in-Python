92794b8ae30860515c3ad77da25fec1fecb086b7
Pratik <kokatepratik20@gmail.com>
2026-07-17 23:29:19 +0530

    Add Array Insertion implementation

---
+++ Arrays/02_Array_Insertion.py
@@ -0,0 +1,21 @@
+"""
+Program: Array Insertion
+
+Description:
+Insert an element at a specific position in an array.
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(1)
+"""
+
+numbers = [10, 20, 40, 50]
+
+position = 2
+value = 30
+
+numbers.insert(position, value)
+
+print(numbers)
